7f655fb3f6517fe876f2cd64d057d14ffd5d737e
Julius Cinco Cesar <157534723+knznsmn@users.noreply.github.com>
2024-08-14 21:28:05 +0400

    Add first prompt

---
+++ js100/001/main.js
@@ -0,0 +1,24 @@
+// Bindings
+const _ = {
+	blockquote: document.getElementsByTagName('blockquote')[0],
+	quote: document.getElementsByTagName('p')[0],
+	author: document.getElementsByTagName('p')[1],
+	cite: document.getElementsByTagName('cite')[0],
+}
+
+const quotations = [
+{
+	author: 'Albert Einstein',
+	quote: 'Tiktok is stupid.',
+	img: 'https://www.github.com/knznsmn.png',
+},
+{
+	author: 'Julius Cesar',
+	quote: 'Indeed, Tiktok is stupid.',
+	img: 'https://www.github.com/jccreatifs',
+},
+]
+// Function Hall
+
+// main()
+_.blockquote.innerHTML = 'Weye';
